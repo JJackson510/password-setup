a1743084e3c3fcd4c8ebef05103241442f6d0d78
JJackson510 <jamiej.jj5@gmail.com>
2021-12-04 19:45:02 -0600

    password requirements

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,100 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "1234567890";
+const characters = "!@#$%^&*(){}[]=<>/,.";
+
+function randomCharFromString(stringValue) {
+  var randomNumber = Math.floor(Math.random() * stringValue.length);
+  return stringValue[randomNumber];
+}
+
+function passwordLength() {
+  var length;
+  var isLengthvalid = false;
+
+  while (!isLengthvalid) {
+    var lengthPrompt = parseInt(
+      window.prompt(
+        "How many characters would you like the password to be? Must be between 8 and 128."
+      )
+    );
+
+    if (!lengthPrompt) {
+      alert("Length is required to create a password");
+    } else if (lengthPrompt < 8 || lengthPrompt > 128) {
+      alert("You must choose between 8 and 128");
+    } else {
+      length = lengthPrompt;
+      isLengthvalid = true;
+    }
+  }
+
+  return length;
+}
+function passwordCriteriaWithoutPrompts() {
+  var criteriaArr = [];
+  alert("Password will have numbers, upper case letters, lower case letters, and characters");
+  criteriaArr.push(numbers);
+  criteriaArr.push(characters);
+  criteriaArr.push(lowerCaseLetters);
+  criteriaArr.push(upperCaseLetters);
+
+  return criteriaArr;
+}
+function passwordCriteria() {
+  var criteriaArr = [];
+  var isCriteriaValid = false;
+
+  while (!isCriteriaValid) {
+    var confirmNumber = window.confirm("Will this have numbers?");
+    var confirmCharacter = window.confirm("Will this have characters?");
+    var confirmLowerCase = window.confirm("Will this have lower case letters?");
+    var confirmUpperCase = window.confirm("Will this have upper case letters?");
+
+    if (
+      !confirmNumber &&
+      !confirmCharacter &&
+      !confirmLowerCase &&
+      !confirmUpperCase
+    ) {
+      alert("Must have one of each character!");
+    } else {
+      if (confirmNumber) {
+        criteriaArr.push(numbers);
+      }
+      if (confirmCharacter) {
+        criteriaArr.push(characters);
+      }
+      if (confirmLowerCase) {
+        criteriaArr.push(lowerCaseLetters);
+      }
+      if (confirmUpperCase) {
+        criteriaArr.push(upperCaseLetters);
+      }
+      isCriteriaValid = true;
+    }
+  }
+
+  return criteriaArr;
+}
+
+function generatePassword() {
+  var length = passwordLength();
+  var criteriaArr = passwordCriteriaWithoutPrompts();
+  var password = [];
+
+  for (let index = 0; index < length; index++) {
+    var criteria = criteriaArr[Math.floor(Math.random() * criteriaArr.length)];
+    password.push(randomCharFromString(criteria));
+  }
+
+  return password.join("");
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -10,8 +102,13 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// st up prompt for password criteria
+// set up lenght of the password
+// confirm characters you want to use
+//set up charracters
+//
